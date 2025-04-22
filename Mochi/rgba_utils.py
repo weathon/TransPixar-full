8e3676c6643b7cbeafe4804abbb43e3294d545f4
Marshall <wguo6358@gmail.com>
2025-04-22 12:17:10 -0700

    Update rgba_utils.py

--- Mochi/rgba_utils.py
+++ Mochi/rgba_utils.py
@@ -66,16 +66,16 @@ class RGBALoRAMochiAttnProcessor:
     def _apply_lora(self, hidden_states, seq_len, query, key, value, scaling):
         """Applies LoRA updates to query, key, and value tensors."""
         query_delta = self.to_q_lora(hidden_states).to(query.device)
-        query[:, -seq_len // 2:, :] += query_delta[:, -seq_len // 2:, :] * scaling
-        # query += query_delta * scaling
+        # query[:, -seq_len // 2:, :] += query_delta[:, -seq_len // 2:, :] * scaling
+        query += query_delta * scaling
 
         key_delta = self.to_k_lora(hidden_states).to(key.device)
-        key[:, -seq_len // 2:, :] += key_delta[:, -seq_len // 2:, :] * scaling
-        # key += key_delta * scaling
+        # key[:, -seq_len // 2:, :] += key_delta[:, -seq_len // 2:, :] * scaling
+        key += key_delta * scaling
 
         value_delta = self.to_v_lora(hidden_states).to(value.device)
-        value[:, -seq_len // 2:, :] += value_delta[:, -seq_len // 2:, :] * scaling
-        # value += value_delta * scaling
+        # value[:, -seq_len // 2:, :] += value_delta[:, -seq_len // 2:, :] * scaling
+        value += value_delta * scaling
 
         return query, key, value
 
@@ -187,8 +187,8 @@ class RGBALoRAMochiAttnProcessor:
         # linear proj
         original_hidden_states = attn.to_out[0](hidden_states)
         hidden_states_delta = self.to_out_lora(hidden_states).to(hidden_states.device)
-        # original_hidden_states += hidden_states_delta * scaling
-        original_hidden_states[:, -sequence_length // 2:, :] += hidden_states_delta[:, -sequence_length // 2:, :] * scaling
+        original_hidden_states += hidden_states_delta * scaling
+        # original_hidden_states[:, -sequence_length // 2:, :] += hidden_states_delta[:, -sequence_length // 2:, :] * scaling
         # dropout
         hidden_states = attn.to_out[1](original_hidden_states)
 
