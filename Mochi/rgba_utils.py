bc259573d01815e9702a8c9073f0296827f6f796
weathon <wg25r@student.ubc.ca>
2025-04-23 05:13:23 +0000

    max norm

--- Mochi/rgba_utils.py
+++ Mochi/rgba_utils.py
@@ -69,7 +69,7 @@ class RGBALoRAMochiAttnProcessor:
         self.to_rgb_out_lora = create_lora_layer(latent_dim, lora_rank, latent_dim)
         
         # self.domain_embeding = nn.parameter.Parameter(torch.randn(latent_dim) * 0.1).cuda()
-        self.domain_embeding = nn.Embedding(1, latent_dim).cuda()
+        self.domain_embeding = nn.Embedding(1, latent_dim, max_norm=0.5).cuda()
 
     def _apply_lora(self, hidden_states, seq_len, query, key, value, scaling):
         """Applies LoRA updates to query, key, and value tensors."""
