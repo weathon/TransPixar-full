0455ed2764466cd71152adab92e107d2090f74c1
weathon <wg25r@student.ubc.ca>
2025-04-24 21:25:47 +0000

    fix typo

--- Mochi/cat.py
+++ Mochi/cat.py
@@ -1,11 +1,11 @@
 import torch
 import os
-files = os.listdir("video_rgb")
+files = os.listdir("video_rgb_")
 os.makedirs("final_dataset", exist_ok=True)
 for file in files:
     if not file.endswith("latent.pt"):
         continue
-    tensor1 = torch.load("video_rgb/"+file)["ldist"]
+    tensor1 = torch.load("video_rgb_/"+file)["ldist"]
     tensor2 = torch.load("video_alpha/"+file)["ldist"]
     assert (torch.abs(tensor1 - tensor2) > 0.01).any()
     res = torch.cat([tensor1[:], tensor2[:]], dim=2)
