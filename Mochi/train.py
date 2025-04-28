2da92d622e80a9d5bf5171a93840a0c617882e1b
weathon <wguo6358@gmail.com>
2025-04-27 18:41:09 -0700

    touyune

--- Mochi/train.py
+++ Mochi/train.py
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+
+# maybe I need negative prompt during training because it might not know what is bad 
+
 import gc
 import random
 from glob import glob
